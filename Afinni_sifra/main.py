cdc5c5bdb3d481d5e279e19ae692caf529fe9a86
MarekBereznaj <55427090+MarekBereznaj@users.noreply.github.com>
2023-12-09 21:46:00 +0100

    Update main.py
    Chyba málej jsem se odjebal nože

--- Afinni_sifra/main.py
+++ Afinni_sifra/main.py
@@ -1,180 +1,146 @@
 import tkinter as tk
+from math import gcd
+import string
 import unicodedata
 
-def remove_duplicates(text):
-    return "".join(sorted(set(text), key=text.index))
+# Inicializace hlavního okna
+root = tk.Tk()
+root.geometry("800x800")
+root.title("Afinní šifra")
 
-def remove_spaces(text):
-    return text.replace(" ", "")
+# Vytvoření labelu a textového pole pro klíč A
+label_A = tk.Label(root, text="Klíč A", font=('Arial', 18))
+label_A.pack(padx=20, pady=20)
+textbox_A = tk.Text(root, height=1, font=('Arial', 16))
+textbox_A.pack()
 
+# Vytvoření labelu a textového pole pro klíč B
+label_B = tk.Label(root, text="Klíč B", font=('Arial', 18))
+label_B.pack(padx=20, pady=20)
+textbox_B = tk.Text(root, height=1, font=('Arial', 16))
+textbox_B.pack()
+
+# Label pro zadání textu k šifrování
+label_text = tk.Label(root, text="Zadejte text k zašifrování", font=('Arial', 18))
+label_text.pack(padx=20, pady=20)
+textbox_text = tk.Text(root, height=1, font=('Arial', 16))
+textbox_text.pack()
+
+# Label pro zobrazení dešifrovaného textu
+desifrovan_label = tk.Label(root, text="")
+desifrovan_label.pack(pady=10)
+
+# Label pro zadání textu k dešifrování
+label_text_d = tk.Label(root, text="Zadejte text k dešifrování", font=('Arial', 18))
+label_text_d.pack(padx=20, pady=20)
+textbox_text_d = tk.Text(root, height=1, font=('Arial', 16))
+textbox_text_d.pack()
+
+# Label pro zobrazení výsledku
+my_label = tk.Label(root, text="")
+my_label.pack(pady=20)
+
+# Funkce pro odstranění nepotřebných znaků z textu
+def fixtext(strng):
+    CHARS_TO_REMOVE = '''!ˇ´()-[]{};:'",<>./?@#$%^&*_~'''
+
+    for char in CHARS_TO_REMOVE:
+        strng = strng.replace(char, "")
+
+    strng = str.lower(strng)
+    return strng
+
+# Funkce pro normalizaci a odstranění diakritiky
 def coding(strng):
     strng = unicodedata.normalize('NFD', strng)
     strng = u"".join([c for c in strng if not unicodedata.combining(c)])
-    strng = remove_spaces(strng)
     return strng
-
-def fixed_text(strng):
+# Funkce pro odstranění speciálních znaků a volání funkce pro normalizaci znaků
+def fixedtext(strng):
     CHARS_TO_REMOVE = '''!ˇ´()-[]{};:'",<>./?@#$%^&*_~'''
+
     for char in CHARS_TO_REMOVE:
         strng = strng.replace(char, "")
+
     strng = str.lower(strng)
-    strng = coding(strng)
+    strng=coding(strng)
     return strng
+# Funkce pro zašifrování textu
+def get_text():
+    my_label.config(text="")  # Vymazání výstupního labelu
+    OT = textbox_text.get(1.0, "end-1c")
+    a = int(textbox_A.get(1.0, "end-1c"))
+    b = int(textbox_B.get(1.0, "end-1c"))
+    OT = fixedtext(OT)
 
-def create_playfair_table(key):
-    key = remove_duplicates(key)
-    keyT = [['' for _ in range(5)] for _ in range(5)]
-    alphabet = 'abcdefghiklmnopqrstuvwxyz'  # 'j' is omitted
-    key += alphabet
+    if gcd(a, 26) != 1 or not (1 <= a <= 25):
+        my_label.config(text="Neplatný klíč A. Klíč A musí být nesoudělný s 26 a v rozmezí [1, 25].")
+        return
 
-    i, j = 0, 0
-    for char in key:
-        keyT[i][j] = char
-        j += 1
-        if j == 5:
-            i += 1
-            j = 0
-        if i == 5:
-            break
+    encrypted_text = ""
+    for j in OT:
+        if j.isspace():
+            encrypted_text += "XMEZERAX"
+        elif j.isalpha():
+            output = (a * (ord(j) - (65 + (32 * (ord(j) > 96)))) + b) % 26
+            encrypted_text += string.ascii_uppercase[output]
+        elif j.isdigit():
+            encrypted_text += str((int(j) + b) % 10)
 
-    return keyT
+    from textwrap import wrap
+    encrypted_text = wrap(encrypted_text, 5)
+    encrypted_text = " ".join(encrypted_text)
+    my_label.config(text=encrypted_text)
 
-def search(keyT, a, b):
-    arr = [0, 0, 0, 0]
-    if a == 'w':
-        a = 'v'
-    elif b == 'w':
-        b = 'v'
-    for i in range(5):
-        for j in range(5):
-            if keyT[i][j] == a:
-                arr[0], arr[1] = i, j
-            elif keyT[i][j] == b:
-                arr[2], arr[3] = i, j
-    return arr
+# Funkce pro dešifrování textu
+def desifruj_text():
+    desifrovan_label.config(text="")
+    a = int(textbox_A.get(1.0, "end-1c"))
+    b = int(textbox_B.get(1.0, "end-1c"))
+    desifrovan = textbox_text_d.get(1.0, "end-1c")
+    desifrovan = str.lower(desifrovan)
+    add = ""
+    desifrovan = desifrovan.replace(" ", "")
 
-def mod5(a):
-    if a < 0:
-        a += 5
-    return a % 5
+    desifrovan = desifrovan.replace("xmezerax", " ")
 
-def encrypt(str, keyT):
-    ps = len(str)
+    for h in desifrovan:
+        if h.isspace():
+            add += " "
+        elif h.isalpha():
+            alphabet = string.ascii_lowercase
+            ot = alphabet.find(h)
+            position = (pow(a, -1, 26) * (ot - b)) % 26
+            add += string.ascii_uppercase[position]
+        elif h.isdigit():
+            add += str((int(h) - b) % 10)
 
-    # If the length is odd, append a dummy character
-    if ps % 2 == 1:
-        str += 'x'
+    desifrovan_label.config(text=add)
 
-    i = 0
-    encrypted_pairs = []
-    while i < ps:
-        # Check if i + 1 is within the bounds of the string
-        if i + 1 < ps:
-            a = search(keyT, str[i], str[i + 1])
-            if a[0] == a[2]:
-                encrypted_pairs.append(keyT[a[0]][mod5(a[1] + 1)] + keyT[a[0]][mod5(a[3] + 1)])
-            elif a[1] == a[3]:
-                encrypted_pairs.append(keyT[mod5(a[0] + 1)][a[1]] + keyT[mod5(a[2] + 1)][a[1]])
-            else:
-                encrypted_pairs.append(keyT[a[0]][a[3]] + keyT[a[2]][a[1]])
-            i += 2
-        else:
-            # If i + 1 is out of bounds, handle the last character
-            a = search(keyT, str[i], 'x')  # Assume 'x' as the dummy character
-            encrypted_pairs.append(keyT[a[0]][mod5(a[1] + 1)] + 'x')
-            break
+# Funkce pro kopírování výstupu z šifrování
+def copy_encryption_output():
+    root.clipboard_clear()
+    root.clipboard_append(my_label.cget("text"))
+    root.update()
 
-    return " ".join(encrypted_pairs)
+# Funkce pro kopírování výstupu z dešifrování
+def copy_decryption_output():
+    root.clipboard_clear()
+    root.clipboard_append(desifrovan_label.cget("text"))
+    root.update()
 
-def decrypt(str, keyT):
-    ps = len(str)
-
-    i = 0
-    decrypted_pairs = []
-    while i < ps:
-        # Check if i + 1 is within the bounds of the string
-        if i + 1 < ps:
-            a = search(keyT, str[i], str[i + 1])
-            if a[0] == a[2]:
-                decrypted_pairs.append(keyT[a[0]][mod5(a[1] - 1)] + keyT[a[0]][mod5(a[3] - 1)])
-            elif a[1] == a[3]:
-                decrypted_pairs.append(keyT[mod5(a[0] - 1)][a[1]] + keyT[mod5(a[2] - 1)][a[1]])
-            else:
-                decrypted_pairs.append(keyT[a[0]][a[3]] + keyT[a[2]][a[1]])
-            i += 2
-        else:
-            # If i + 1 is out of bounds, handle the last character
-            a = search(keyT, str[i], 'x')  # Assume 'x' as the dummy character
-            decrypted_pairs.append(keyT[a[0]][mod5(a[1] - 1)] + 'x' if str[i] == 'x' else keyT[a[0]][mod5(a[1] - 1)])
-            break
-
-    return "".join(decrypted_pairs)
-
-def decrypt_by_playfair_cipher(str, key):
-    ks = len(key)
-    key = fixed_text(key)
-    str = fixed_text(str)
-    keyT = create_playfair_table(key)
-    return decrypt(str, keyT)
-
-def encrypt_by_playfair_cipher(str, key):
-    ks = len(key)
-    key = fixed_text(key)
-    str = fixed_text(str)
-    keyT = create_playfair_table(key)
-
-    # Display the encryption table in the GUI
-    key_table_text = "\n".join([" ".join(row) for row in keyT])
-    key_table_label.config(text=f"Šifrovací tabulka:\n{key_table_text}")
-
-    return encrypt(str, keyT)
-
-def encrypt_command():
-    key = textbox_key.get()
-    text = textbox_text.get()
-    result = encrypt_by_playfair_cipher(text, key)
-    encrypted_result_label.config(text=result)
-
-def decrypt_and_display_command():
-    key = textbox_key.get()
-    text = textbox_text_decrypt.get()
-    result = decrypt_by_playfair_cipher(text, key)
-    decrypted_result_label.config(text=result)
-
-root = tk.Tk()
-root.geometry("800x800")
-root.title("Playfair")
-
-label_key = tk.Label(root, text="Klíč", font=('Arial', 18))
-label_key.pack(padx=20, pady=20)
-textbox_key = tk.Entry(root, font=('Arial', 16))  # Change from Text to Entry
-textbox_key.pack()
-
-label_text = tk.Label(root, text="Zadejte text k zašifrování", font=('Arial', 18))
-label_text.pack(padx=20, pady=20)
-textbox_text = tk.Entry(root, font=('Arial', 16))  # Change from Text to Entry
-textbox_text.pack()
-
-label_text_decrypt = tk.Label(root, text="Zadejte dešifrování", font=('Arial', 18))
-label_text_decrypt.pack(padx=20, pady=20)
-textbox_text_decrypt = tk.Entry(root, font=('Arial', 16))  # Change from Text to Entry
-textbox_text_decrypt.pack()
-
-encrypted_result_label = tk.Label(root, text="", font=('Arial', 16))
-encrypted_result_label.pack(pady=20)
-
-decrypted_result_label = tk.Label(root, text="", font=('Arial', 16))
-decrypted_result_label.pack(pady=20)
-
-# Add label for displaying encryption table
-key_table_label = tk.Label(root, text="", font=('Arial', 16))
-key_table_label.pack(pady=20)
-
-Button_encrypt = tk.Button(root, text="Zašifrovat", command=encrypt_command)
+# Vytvoření tlačítek pro šifrování, dešifrování a kopírování výstupu
+Button_encrypt = tk.Button(root, text="Zašifrovat", command=get_text)
 Button_encrypt.pack(side=tk.LEFT, padx=20, pady=20)
 
-# Add decryption functionality
-Button_decrypt = tk.Button(root, text="Dešifrovat", command=decrypt_and_display_command)
+Button_decrypt = tk.Button(root, text="Dešifrovat", command=desifruj_text)
 Button_decrypt.pack(side=tk.LEFT, padx=20, pady=20)
 
+Button_copy_encrypt = tk.Button(root, text="Kopírovat výstup z šifrování", command=copy_encryption_output)
+Button_copy_encrypt.pack(side=tk.LEFT, padx=20, pady=20)
+
+Button_copy_decrypt = tk.Button(root, text="Kopírovat výstup z dešifrování", command=copy_decryption_output)
+Button_copy_decrypt.pack(side=tk.LEFT, padx=20, pady=20)
+
+# Spuštění hlavní smyčky
 root.mainloop()
